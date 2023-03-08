80acb27568695af2ba7c7805891bf5fdff36be09
Imad Bouziani <oumdadzn@gmail.com>
2023-03-08 23:17:38 +0800

    Update jotai.ts
    actually, we can pass the "computeValue" function straight to `atom.subscribe`.
    + extra simplification of the code.
    TESTED

--- src/jotai.ts
+++ src/jotai.ts
@@ -1,73 +1,59 @@
 import { useSyncExternalStore } from "react";
 
 interface Atom<AtomType> {
-  get: () => AtomType;
-  set: (newValue: AtomType) => void;
-  subscribe: (callback: (newValue: AtomType) => void) => () => void;
-  _subscribers: () => number;
+    get: () => AtomType;
+    set: (newValue: AtomType) => void;
+    subscribe: (callback: (newValue: AtomType) => void) => () => void;
+    _subscribers: () => number;
 }
 
 type AtomGetter<AtomType> = (
-  get: <Target>(a: Atom<Target>) => Target
+    get: <Target>(a: Atom<Target>) => Target
 ) => AtomType;
 
 export function atom<AtomType>(
-  initialValue: AtomType | AtomGetter<AtomType>
+    initialValue: AtomType | AtomGetter<AtomType>
 ): Atom<AtomType> {
-  let value: AtomType =
-    typeof initialValue === "function" ? (null as AtomType) : initialValue;
+    let value: AtomType = initialValue as AtomType
 
-  const subscribers = new Set<(newValue: AtomType) => void>();
-  const subscribed = new Set<Atom<any>>();
+    const subscribers = new Set<(newValue: AtomType) => void>();
 
-  function get<Target>(atom: Atom<Target>) {
-    let currentValue = atom.get();
-    // console.log(atom._subscribers())
-
-    if (!subscribed.has(atom)) {
-      subscribed.add(atom);
-      atom.subscribe(function (newValue) {
-        if (currentValue === newValue) return;
-        currentValue = newValue;
-        computeValue();
-      });
+    function get<Target>(atom: Atom<Target>) {
+        atom.subscribe(computeValue);
+        return atom.get();
     }
-    
-    return currentValue;
-  }
 
-  async function computeValue() {
-    const newValue =
-      typeof initialValue === "function"
-        ? (initialValue as AtomGetter<AtomType>)(get)
-        : value;
-    value = (null as AtomType);
-    value = await newValue;
-    subscribers.forEach((callback) => callback(value));
-  }
+    async function computeValue() {
+        value =
+            typeof initialValue === "function"
+                ? await (initialValue as AtomGetter<AtomType>)(get)
+                : value;
 
-  computeValue();
+        subscribers.forEach((callback) => callback(value));
+    }
 
-  return {
-    get: () => value,
-    set: (newValue) => {
-      value = newValue;
-      computeValue();
-    },
-    subscribe: (callback) => {
-      subscribers.add(callback);
-      return () => {
-        subscribers.delete(callback);
-      };
-    },
-    _subscribers: () => subscribers.size,
-  };
+    computeValue();
+
+    return {
+        get: () => value,
+        set: (newValue) => {
+            value = newValue;
+            computeValue();
+        },
+        subscribe: (callback) => {
+            subscribers.add(callback);
+            return () => {
+                subscribers.delete(callback);
+            };
+        },
+        _subscribers: () => subscribers.size,
+    };
 }
 
 export function useAtom<AtomType>(atom: Atom<AtomType>) {
-  return [useSyncExternalStore(atom.subscribe, atom.get), atom.set];
+    return [useSyncExternalStore(atom.subscribe, atom.get), atom.set];
 }
 
 export function useAtomValue<AtomType>(atom: Atom<AtomType>) {
-  return useSyncExternalStore(atom.subscribe, atom.get);
+    return useSyncExternalStore(atom.subscribe, atom.get);
 }
